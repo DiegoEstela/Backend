4dac16e11449618d5be57d22d0555fadec854638
Diego Ezequiel Estela <die.estela@gmail.com>
2021-10-13 01:20:34 -0300

    Commit para revision

--- src/Clase4/Clase4.js
+++ src/Clase4/Clase4.js
@@ -1,14 +1,7 @@
 const fs = require("fs");
 
-let fileR = fs.readFileSync("./test.txt", "utf-8");
-
-// Escritura
-
-let fileW = fs.writeFileSync("./test2.txt", "Nuevo Texto", "utf-8");
-
-// Apend
 const id = [];
-
+const productos = [];
 class Contenedor {
   constructor(archivo) {
     this.archivo = archivo;
@@ -31,13 +24,26 @@ class Contenedor {
   getById = async () => {
     try {
       let read = await fs.promises.readFile("./producto.txt", "utf-8");
-      let productos = [];
       productos.push(read);
       console.log(productos);
     } catch (err) {
       console.log(err);
     }
   };
+
+  getall = async () => {
+    try {
+      let read = await fs.promises.readFile("./producto.txt", "utf-8");
+      console.log("todos los productos encontrasdos son: ", read);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  deleteall() {
+    fs.unlink("./producto.txt", (err) => {
+      if (err) throw "Error al borrar ";
+    });
+  }
 }
 
 const producto = new Contenedor("./producto.txt");
@@ -46,4 +52,12 @@ producto.save({ title: "escuadra", price: 123.34 });
 
 producto.save({ title: "regla", price: 12.3 });
 
+producto.save({ title: "Lapicera", price: 45.3 });
+
 producto.getById();
+
+producto.getall();
+
+/*borrar producto
+producto.deleteall();
+*/
