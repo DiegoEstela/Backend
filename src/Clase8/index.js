99ff8c11cf0ef7bc032c085738c4df7d3b7cbb64
Diego Ezequiel Estela <die.estela@gmail.com>
2021-10-27 21:32:59 -0300

    Se utilizo layout, views y partials

--- src/Clase8/index.js
+++ src/Clase8/index.js
@@ -2,9 +2,34 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
-const port = 8080;
-
-app.set("views", "./view");
+const port = 3030;
+const arr = [
+  {
+    title: "banana",
+    price: 200,
+    thumbnail: "https://unsplash.com/photos/sf_1ZDA1YFw",
+    id: 1,
+  },
+  {
+    title: "Manzana",
+    price: 500,
+    thumbnail: "https://unsplash.com/photos/nmpi584DlCo",
+    id: 2,
+  },
+  {
+    title: "Frutilla",
+    price: 150,
+    thumbnail: "https://unsplash.com/photos/MDZOGRtfuAg",
+    id: 3,
+  },
+  {
+    title: "naranja",
+    price: 200,
+    thumbnail: "https://unsplash.com/photos/bHCERo3jLUU",
+    id: 4,
+  },
+];
+app.set("views", "./views");
 app.set("view engine", "hbs");
 // configuracion de la plantilla
 
@@ -12,10 +37,19 @@ app.engine(
   ".hbs",
   handlebars({
     extname: "hbs",
-    layoutsDir: "/views/layouts",
+    layoutsDir: __dirname * "/views/layouts",
     defaultLayout: "index",
+    partialsDir: __dirname * "/views/partials",
   })
 );
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.get("/productos", (req, res) => {
+  res.render("productos", { layaout: "home", data: arr });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
